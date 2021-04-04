e062ffe969ff727f2557d962f1396a8f14e2e4ab
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-04-03 23:40:52 -0400

    Added windowActions() to scripts.js

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,9 @@
+// async pauses windowActions() until the await promise fulfills
+async function windowActions() {
+  console.log("window loaded");
+  // const endpoint =
+  const request = await fetch("/api/dining/");
+  const diningData = await request.json();
+}
 
+window.onload = windowActions;
