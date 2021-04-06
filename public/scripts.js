54f0ba42ce6b9b73d423e7e5802ed1abd0cf0b16
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-04-05 21:20:29 -0400

    Added classes to index.html, created diningTable using diningData

--- public/scripts.js
+++ public/scripts.js
@@ -4,6 +4,18 @@ async function windowActions() {
   // const endpoint =
   const request = await fetch("/api/dining/");
   const diningData = await request.json();
-}
+  const diningTable = document.getElementById("table");
 
+  diningData.data.forEach((hall) => {
+    const hallInfo = document.createElement("tr");
+    hallInfo.innerHTML = `
+        <td>${hall.hall_id}</td>
+        <td>${hall.hall_name}</td>  
+        <td>${hall.hall_address}</td>`;
+    diningTable.append(hallInfo); // adds hallInfo tr to diningTable
+  });
+}
 window.onload = windowActions;
+
+// ${} is string formatting
+// innerHTML gets or sets HTML within an element
